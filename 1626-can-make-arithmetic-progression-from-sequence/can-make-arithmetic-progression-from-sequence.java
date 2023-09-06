be811336ca481eeb06eb00f227e57be34d42f876
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-06 20:11:39 +0530

    Time: 1 ms (98.48%) | Memory: 40.9 MB (40.64%) - LeetSync

--- 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
@@ -1,12 +1,30 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int dif=arr[1]-arr[0];
-        for(int i=1;i<arr.length-1;i++){
-            if(arr[i+1]-arr[i] != dif){
-                return false;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int n=arr.length;
+        for(int val:arr){
+            if(val<min){
+                min=val;
+            }
+            if(val>max){
+                max=val;
             }
         }
-        return true;
+        if(max-min==0){
+            return true;
+        }
+        if((max-min)%(n-1)!=0){
+            return false;
+        }
+        int diff=(max-min)/(n-1);
+        Set<Integer> set= new HashSet<>();
+        for(int val:arr){
+            if((val-min) % diff != 0){
+                return false;
+            }
+            set.add(val);
+        }
+        return set.size()==n;
     }
 }
