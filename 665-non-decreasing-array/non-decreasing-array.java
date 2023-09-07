2509eeed467bc939d58c4e06d91dd02eb88b4428
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-07 09:45:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (5.91%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int cnt=0;
+        for(int i=1;i<nums.length;i++){
+            // System.out.println(Arrays.toString(nums));
+            if(nums[i] < nums[i-1]){
+                cnt++;
+                if(cnt>1)return false;
+                if(i>=2 && nums[i-2]>nums[i]){
+                    nums[i]=nums[i-1];
+                }else{
+                    nums[i-1]=nums[i];
+                }
+            }
+        }
+        return true;
+    }
+}
