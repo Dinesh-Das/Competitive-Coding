0bf5d7c6a6bdad5ca546157ff157e4d3db973ef0
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-10 09:45:18 +0530

    Time: 12 ms (55.01%) | Memory: 44.6 MB (6.24%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,31 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> ransom = new HashMap<>();
-        HashMap<Character,Integer> mag=new HashMap<>();
-        for(char ch:ransomNote.toCharArray()){
-            if(ransom.containsKey(ch)){
-                ransom.put(ch,ransom.get(ch)+1);
+        for(int i=0;i<ransomNote.length();i++){
+            if(magazine.indexOf(ransomNote.charAt(i)) == -1){
+                return false;
             }else{
-                ransom.put(ch,1);
+                magazine=magazine.substring(0,magazine.indexOf(ransomNote.charAt(i))) +magazine.substring(magazine.indexOf(ransomNote.charAt(i)) +1 ); 
             }
         }
-        for(char ch:magazine.toCharArray()){
-            if(mag.containsKey(ch)){
-                mag.put(ch,mag.get(ch)+1);
-            }else{
-                mag.put(ch,1);
-            }
-        }
-        boolean charPresent=true;
-        for(Map.Entry<Character,Integer> entry:ransom.entrySet()){
-            if(mag.containsKey(entry.getKey())){
-                if(mag.get(entry.getKey()) < entry.getValue()){
-                    return false;
-                }
-            }else{
-                charPresent=false;
-            }
-        }
-        return charPresent;
+        return true;
     }
 }
