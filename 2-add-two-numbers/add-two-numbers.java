525fd88d160d6dbe8e3e7b37e3337d37fa2a22e5
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-10 21:28:50 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (41.64%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -8,38 +8,62 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.BigInteger;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1=new Stack<>();
-        Stack<Integer> s2=new Stack<>();
-        while(l1!=null){
-            s1.push(l1.val);
+        ListNode cur=new ListNode();
+        ListNode result=cur;
+        int carry=0;
+        while(l1!=null && l2!=null){
+            int sum=l1.val+l2.val+carry;
+            int val=0;
+            if(sum>=10){
+                val=sum%10;
+                carry=1;
+            }else{
+                val=sum;
+                carry=0;
+            }
+            ListNode node=new ListNode(val);
+            cur.next=node;
+            cur=cur.next;
             l1=l1.next;
-        }
-        while(l2!=null){
-            s2.push(l2.val);
             l2=l2.next;
         }
-        BigInteger a = new BigInteger("0");
-        BigInteger b = new BigInteger("0");
-        while(!s1.isEmpty()){
-            a=a.multiply(new BigInteger("10")).add(new BigInteger(String.valueOf(s1.pop())));
+        while(l2!=null){
+            int sum=l2.val+carry;
+            int val=0;
+            if(sum>=10){
+                val=sum%10;
+                carry=1;
+            }else{
+                val=sum;
+                carry=0;
+            }
+            ListNode node=new ListNode(val);
+            cur.next=node;
+            cur=cur.next;
+            l2=l2.next;
         }
-        while(!s2.isEmpty()){
-           b=b.multiply(new BigInteger("10")).add(new BigInteger(String.valueOf(s2.pop())));
+        while(l1!=null){
+            int sum=l1.val+carry;
+            int val=0;
+            if(sum>=10){
+                val=sum%10;
+                carry=1;
+            }else{
+                val=sum;
+                carry=0;
+            }
+            ListNode node=new ListNode(val);
+            cur.next=node;
+            cur=cur.next;
+            l1=l1.next;
         }
-        BigInteger sum= a.add(b);
-        String ans=sum.toString();
-        int i=ans.length()-1;
-        ListNode added= new ListNode();
-        ListNode result=added;
-        while(i>=0){
-            ListNode node=new ListNode(Integer.parseInt(""+ans.charAt(i)));
-            added.next=node;
-            added=added.next;
-            i--;
+        if(carry==1){
+            ListNode node=new ListNode(carry);
+            cur.next=node;
+            cur=cur.next;
         }
-        return result.next; 
+        return result.next;
     }
 }
