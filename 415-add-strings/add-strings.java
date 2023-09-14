044cea05622557dee6f3268503db98eb9829f798
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-14 09:55:49 +0530

    Time: 2 ms (74.98%) | Memory: 41.5 MB (78.46%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -3,41 +3,21 @@ class Solution {
         int i=num1.length()-1,j=num2.length()-1;
         StringBuilder sb= new StringBuilder();
         int carry=0;
-        while(i>=0 && j>=0){
-            int sum=Character.getNumericValue(num1.charAt(i)) + Character.getNumericValue(num2.charAt(j)) + carry;
+        while(i>=0 || j>=0){
+            int a= i>=0 ? Character.getNumericValue(num1.charAt(i)) :0;
+            int b= j>=0 ? Character.getNumericValue(num2.charAt(j)): 0;
+            int sum= a+b + carry;
             if(sum < 10){
-                sb.append(String.valueOf(sum));
+                sb.append(sum);
                 carry=sum/10;
             }else{
-                sb.append(String.valueOf(sum%10));
+                sb.append(sum%10);
                 carry=sum/10;
             }
             i--;j--;
         }
-        while(i>=0){
-            int sum=Character.getNumericValue(num1.charAt(i)) + carry;
-            if(sum < 10){
-                sb.append(String.valueOf(sum));
-                carry=sum/10;
-            }else{
-                sb.append(String.valueOf(sum%10));
-                carry=sum/10;
-            }
-            i--;
-        }
-        while(j>=0){
-            int sum=Character.getNumericValue(num2.charAt(j)) + carry;
-            if(sum < 10){
-                sb.append(String.valueOf(sum));
-                carry=sum/10;
-            }else{
-                sb.append(String.valueOf(sum%10));
-                carry=sum/10;
-            }
-            j--;
-        }
         if(carry!=0){
-            sb.append(String.valueOf(carry));
+            sb.append(carry);
         }
         return sb.reverse().toString();
     }
