4946cafb946cfd44b71b5303d5d668d3b9ff1979
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-17 21:18:57 +0530

    Time: 3 ms (83.59%) | Memory: 43.5 MB (38.78%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,57 +1,26 @@
 class Solution {
-    public Map<Integer,Integer> fillMap(int[] nums){
-        Map<Integer,Integer> map= new HashMap<>();
-        for(int val:nums) {
-            if(map.containsKey(val)){
-                map.put(val,map.get(val)+1);
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i])){
+                map.put(nums1[i],map.get(nums1[i])+1);
             }else{
-                map.put(val,1);
+                map.put(nums1[i],1);
             }
         }
-        return map;
-    }
-    public int[] intersect(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
-        Map<Integer,Integer> a= fillMap(nums1);
-        Map<Integer,Integer> b= fillMap(nums2);
-        List<Integer> list=new ArrayList();
-        if(n>m){
-            list=isInMap(a,nums2);
-        }else if(n < m){
-            list=isInMap(b,nums1);
-        }else{
-            list=compareTwoEqualList(nums1,nums2);
-        }
-        int[] res=new int[list.size()];
-        for(int i=0;i<list.size();i++) res[i]=list.get(i);
-        return res;
-    }
-    public List<Integer> compareTwoEqualList(int[] a, int[] b){
-       List<Integer> x= new ArrayList<>();
-       for(int val:a){
-           x.add(val);
-       }
-       List<Integer> res=new ArrayList<>();
-       for(int val:b){
-           if(x.contains(val)){
-               res.add(val);
-               x.remove(x.indexOf(val));
-           }
-       }
-       return res;
-    }
-    public List<Integer> isInMap(Map<Integer,Integer> map, int[]nums){
-        List<Integer> list= new ArrayList<>();
-        for(int val:nums){
+        List<Integer> list=new ArrayList<>();
+        for(int val:nums2){
             if(map.containsKey(val)){
                 if(map.get(val) > 0){
-                    map.put(val,map.get(val)-1);
                     list.add(val);
+                    map.put(val,map.get(val)-1);
                 }
             }
         }
-        return list;
+        int[] res=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            res[i]=list.get(i);
+        }
+        return res;
     }
-
 }
