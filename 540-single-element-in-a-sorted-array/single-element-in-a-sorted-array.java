d5098515ec796a924339fa84b15974bb27945355
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-09-21 22:43:36 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (78.54%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,27 +1,17 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-       int left,right;
-       for(int i=0;i<nums.length;i++){
-           left=binarySearch(nums,0,i-1,nums[i]);
-           right=binarySearch(nums,i+1,nums.length-1,nums[i]);
-           if(left == -1 && right == -1){
-               return nums[i];
-           }
-       }
-       return -1;
-    }
-    public int binarySearch(int[] nums, int s,int e,int target){
-        if(s+1 == nums.length) return -1;
-        while(s <= e){
-            int m=s+(e-s)/2;
-            if(nums[m]==target){
-                return m;
-            }else if (nums[m] > target){
-                e=m-1;
+        int s=0,e=nums.length-1;
+        while(s < e){
+            int m = s+(e-s)/2;
+            if(m%2==1){
+                m--;
+            }
+            if(nums[m]!=nums[m+1]){
+                e=m;
             }else{
-                s=m+1;
+                s=m+2;
             }
         }
-        return -1;
+        return nums[e];
     }
 }
