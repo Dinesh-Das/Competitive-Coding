b75ff546cbea3f43badf35390b1851d05bf4f955
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-10-11 23:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (6.73%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res=helper(x,Math.abs(n));
+        return n>=0 ? res : 1/res;
+    }
+    
+    public double helper(double x,int n){
+        if(x==0)return 0;
+        if(n==0)return 1;
+        double res=helper(x,n/2);
+        res=res*res;
+        return n%2==0 ?res:res * x; 
+    }
+}
