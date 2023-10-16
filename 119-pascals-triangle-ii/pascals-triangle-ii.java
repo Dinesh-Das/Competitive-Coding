568fa967dd6ac418cbc754c79eb5b82700f65e0e
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-10-16 07:26:20 +0530

    Time: 1 ms (79.70%) | Memory: 40.1 MB (56.38%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -4,11 +4,10 @@ class Solution {
         for(int i=0;i<=rowIndex;i++){
             List<Integer> list=new ArrayList<>();
             for(int j=0;j<=i;j++){
-                if(j==0 || j==i){
+                if(j==0 || j==i)
                     list.add(1);
-                }else{
+                else
                     list.add(prev.get(j) + prev.get(j-1));
-                }
             }
             prev=list;
         }
