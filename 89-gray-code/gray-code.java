fedec2cfbeb29689cbf589c7a4920ca40a871a64
Dinesh Das <78725972+Dinesh-Das@users.noreply.github.com>
2023-11-14 17:39:53 +0530

    Time: 4 ms (98.59%) | Memory: 49.8 MB (28.38%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<(1<<n);i++){
+            list.add(i^(i>>1));
+        }
+        return list;
+    }
+}
